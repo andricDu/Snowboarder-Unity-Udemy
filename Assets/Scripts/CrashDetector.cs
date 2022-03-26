c97bf70e57def87814eb48653ecaa52822049707
Dusan Andric <dusan.andric@gmail.com>
2022-03-26 15:27:31 -0400

    sound effects

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -8,11 +8,14 @@ public class CrashDetector : MonoBehaviour
 
     [SerializeField] ParticleSystem crashEffect;
     [SerializeField] float delay = 1.0f;
+    [SerializeField] AudioClip crashSFX;
 
     void OnTriggerEnter2D(Collider2D other) {
         if (other.tag == "Ground")
         {
+            FindObjectOfType<PlayerController>().DisableControls();
             crashEffect.Play();
+            GetComponent<AudioSource>().PlayOneShot(crashSFX);
             Invoke("ReloadScene", delay);
         }
     }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,22 +6,77 @@ public class PlayerController : MonoBehaviour
 {
 
     [SerializeField] float torqueAmount = 2.0f;
+    [SerializeField] float boostSpeed = 30f;
+    [SerializeField] float baseSpeed = 15f;
+    [SerializeField] float slowSpeed = 7.5f;
+    [SerializeField] ParticleSystem powderParticles;
+
     Rigidbody2D rb2d;
+    SurfaceEffector2D surfaceEffector2D;
+    bool canMove = true;
 
     // Start is called before the first frame update
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        surfaceEffector2D = FindObjectOfType<SurfaceEffector2D>();
+
     }
 
     // Update is called once per frame
     void Update()
     { 
+        if (canMove)
+        {
+            RotatePlayer();
+            RespondToBoost();
+        }
+    }
+
+    public void DisableControls()
+    {
+        this.canMove = false;
+    }
+
+    void OnCollisionEnter2D(Collision2D other) 
+    {
+        if (other.gameObject.tag == "Ground")
+        {
+            powderParticles.Play();
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D other) 
+    {
+        if (other.gameObject.tag == "Ground")
+        {
+            powderParticles.Stop();
+        }
+    }
+
+    void RotatePlayer() 
+    {
         if(Input.GetKey(KeyCode.LeftArrow))
         {
             rb2d.AddTorque(torqueAmount);
         } else if (Input.GetKey(KeyCode.RightArrow)) {
             rb2d.AddTorque(-torqueAmount);
+        }
+    }
+
+    void RespondToBoost()
+    {
+        if(Input.GetKey(KeyCode.UpArrow))
+        {
+            surfaceEffector2D.speed = boostSpeed;
+        } 
+        else if (Input.GetKey(KeyCode.DownArrow)) 
+        {
+            surfaceEffector2D.speed = slowSpeed;
+        } 
+        else 
+        {
+             surfaceEffector2D.speed = baseSpeed;
         }
     }
 
